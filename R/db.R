b437420522d71e61d5c1d60db60968e9d8f3c3f2
Anthonylarouche <ANTHONY.LAROUCHE.951@GMAIL.COM>
2026-04-11 15:33:55 -0400

    Update db.R

--- R/db.R
+++ R/db.R
@@ -1,6 +1,6 @@
 
 connect_db <- function() {
-  dbConnect(RSQLite::SQLite(), "~/Documents/UFC_prediction/Data/UFC_project.db")
+  dbConnect(RSQLite::SQLite(), "Data/UFC_project.db")
 }
 
 disconnect_db <- function(con) {
